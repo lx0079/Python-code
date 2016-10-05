5702de7fb581aae7ec8d0d30462faed6f6174a73
lx0079 <1095839590@qq.com>
2016-10-05 10:27:08 +0800

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,13 @@
+with open ('/home/sum.txt','r') as f :
+	data = f.readlines()
+	
+	for line in data:
+		num = line.split()
+		numbers_int = map(int, num)
+		print numbers_int
+		print sum(numbers_int)
+		write_str = '%d'%(sum(numbers_int))
+	f.close()
+	with open ('/home/sum1.txt','a') as f1:  
+		f1.write(write_str)
+		f1.close()
